740a86a896c15eaeacbd277492009c8d27af184b
Aldrin Clemente <aldrinclemente@gmail.com>
2016-11-09 15:14:01 +0800

    Added BitmapLoader.RequestOptions

--- library/src/main/java/com/truebanana/bitmap/BitmapLoader.java
+++ library/src/main/java/com/truebanana/bitmap/BitmapLoader.java
@@ -28,6 +28,10 @@ import android.graphics.Bitmap;
 import android.widget.ImageView;
 
 import com.truebanana.http.BitmapResponseListener;
+import com.truebanana.http.HTTPRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple helper class for loading and displaying {@link Bitmap}s.
@@ -35,11 +39,21 @@ import com.truebanana.http.BitmapResponseListener;
  */
 public class BitmapLoader {
     private static BitmapMemCache cache;
+    private static RequestOptions defaultRequestOptions = new RequestOptions();
 
     private static void initializeCacheAsNeeded() {
         if (cache == null) {
             cache = new BitmapMemCache();
         }
+    }
+
+    /**
+     * Sets the default {@link RequestOptions} to use for all requests.
+     *
+     * @param options
+     */
+    public static void setDefaultRequestOptions(RequestOptions options) {
+        BitmapLoader.defaultRequestOptions = options;
     }
 
     /**
@@ -49,7 +63,18 @@ public class BitmapLoader {
      * @param imageView The {@link ImageView} where the {@link Bitmap} will be displayed
      */
     public static void displayBitmap(final String url, final ImageView imageView) {
-        displayBitmap(url, 0, 0, imageView);
+        displayBitmap(url, imageView, defaultRequestOptions);
+    }
+
+    /**
+     * Asynchronously decodes a {@link Bitmap} from a URL and displays it in an {@link ImageView}.
+     *
+     * @param url       The URL of the {@link Bitmap} to display
+     * @param imageView The {@link ImageView} where the {@link Bitmap} will be displayed
+     * @param options   The {@link RequestOptions} to use for this request
+     */
+    public static void displayBitmap(final String url, final ImageView imageView, RequestOptions options) {
+        displayBitmap(url, 0, 0, imageView, options);
     }
 
     /**
@@ -62,22 +87,85 @@ public class BitmapLoader {
      * @param imageView The {@link ImageView} where the {@link Bitmap} will be displayed
      */
     public static void displayBitmap(final String url, int width, int height, final ImageView imageView) {
+        displayBitmap(url, width, height, imageView, defaultRequestOptions);
+    }
+
+    /**
+     * Asynchronously decodes a {@link Bitmap} from a URL, resizes it first then displays it in an {@link ImageView}.
+     * Use this to save memory when you have to display unnecessarily large {@link Bitmap}s.
+     *
+     * @param url       The URL of the {@link Bitmap} to display
+     * @param width     The desired width, in pixels
+     * @param height    The desired height, in pixels
+     * @param imageView The {@link ImageView} where the {@link Bitmap} will be displayed
+     * @param options   The {@link RequestOptions} to use for this request
+     */
+    public static void displayBitmap(final String url, int width, int height, final ImageView imageView, RequestOptions options) {
         initializeCacheAsNeeded();
         Bitmap bitmap = cache.get(url);
         if (bitmap != null) {
             imageView.setImageBitmap(bitmap);
         } else {
-            BitmapUtils.decodeFromURL(url, new BitmapResponseListener(width, height) {
-                @Override
-                public void onDecodingSuccessful(Bitmap bitmap) {
-                    imageView.setImageBitmap(bitmap);
-                    cache.put(url, bitmap);
-                }
+            HTTPRequest.create(url)
+                    .addHeaders(options.headers)
+                    .setConnectTimeout(options.connectTimeout)
+                    .setReadTimeout(options.readTimeout)
+                    .setSSLVerificationEnabled(options.sslVerification)
+                    .setHTTPResponseListener(new BitmapResponseListener(width, height) {
+                        @Override
+                        public void onDecodingSuccessful(Bitmap bitmap) {
+                            imageView.setImageBitmap(bitmap);
+                            cache.put(url, bitmap);
+                        }
 
-                @Override
-                public void onDecodingFailed() {
-                }
-            });
+                        @Override
+                        public void onDecodingFailed() {
+                        }
+                    })
+                    .executeAsync();
+        }
+    }
+
+    public static class RequestOptions {
+        Map<String, String> headers = new HashMap<>();
+        boolean sslVerification = false;
+        private int readTimeout = 10000;
+        private int connectTimeout = 10000;
+
+        public Map<String, String> getHeaders() {
+            return headers;
+        }
+
+        public RequestOptions setHeaders(Map<String, String> headers) {
+            this.headers = headers;
+            return this;
+        }
+
+        public boolean isSslVerification() {
+            return sslVerification;
+        }
+
+        public RequestOptions setSslVerification(boolean sslVerification) {
+            this.sslVerification = sslVerification;
+            return this;
+        }
+
+        public int getReadTimeout() {
+            return readTimeout;
+        }
+
+        public RequestOptions setReadTimeout(int readTimeout) {
+            this.readTimeout = readTimeout;
+            return this;
+        }
+
+        public int getConnectTimeout() {
+            return connectTimeout;
+        }
+
+        public RequestOptions setConnectTimeout(int connectTimeout) {
+            this.connectTimeout = connectTimeout;
+            return this;
         }
     }
 }
