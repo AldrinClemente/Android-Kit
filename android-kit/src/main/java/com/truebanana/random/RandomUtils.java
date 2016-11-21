4635418ba5024f700524d908458f85aec5350d3a
Aldrin Clemente <aldrinclemente@gmail.com>
2016-11-21 15:28:04 +0800

    Updated RandomUtils

--- android-kit/src/main/java/com/truebanana/random/RandomUtils.java
+++ android-kit/src/main/java/com/truebanana/random/RandomUtils.java
@@ -27,34 +27,112 @@ package com.truebanana.random;
 import java.util.Random;
 
 /**
- * Includes utility methods for generating random values.
+ * Includes utility methods for generating random values. All methods use the same instance of
+ * {@link Random}.
+ * <p>
+ * Do not use this for security and cryptography.
  */
 public class RandomUtils {
-	private static Random random = new Random();
-	
-	public static boolean randomBoolean() {
-		return random.nextBoolean();
-	}
-	
-	public static int randomInt(int min, int max) {
-		return random.nextInt((max - min) + 1) + min;
-	}
-	
-	public static long randomLong(long min, long max) {
-		return (long) ((random.nextDouble() * (max - min) + 1) + min);
-	}
-	
-	public static float randomFloat(float min, float max) {
-		return random.nextFloat() * (max - min) + min;
-	}
-	
-	public static double randomDouble(float min, float max) {
-		return random.nextDouble() * (max - min) + min;
-	}
-	
-	public static byte[] randomDoubleBytes(int length) {
-		byte[] bytes = new byte[length];
-		random.nextBytes(bytes);
-		return bytes;
-	}
+    private static Random random = new Random();
+
+    /**
+     * Returns a random boolean.
+     *
+     * @return
+     */
+    public static boolean randomBoolean() {
+        return random.nextBoolean();
+    }
+
+    /**
+     * Returns a random integer.
+     *
+     * @return
+     */
+    public static int randomInt() {
+        return random.nextInt();
+    }
+
+    /**
+     * Returns a random integer within the specified range.
+     *
+     * @param min The minimum value (inclusive)
+     * @param max The maximum value (inclusive)
+     * @return
+     */
+    public static int randomInt(int min, int max) {
+        return (int) randomLong(min, max);
+    }
+
+    /**
+     * Returns a random long.
+     *
+     * @return
+     */
+    public static long randomLong() {
+        return random.nextLong();
+    }
+
+    /**
+     * Returns a random long within the specified range.
+     *
+     * @param min The minimum value (inclusive)
+     * @param max The maximum value (inclusive)
+     * @return
+     */
+    public static long randomLong(long min, long max) {
+        return (long) (random.nextDouble() * ((max - min) + 1)) + min;
+    }
+
+    /**
+     * Returns a random float between 0 (inclusive) and 1 (exclusive).
+     *
+     * @return
+     */
+    public static float randomFloat() {
+        return random.nextFloat();
+    }
+
+    /**
+     * Returns a random float within the specified range.
+     *
+     * @param min The minimum value (inclusive)
+     * @param max The maximum value (exclusive)
+     * @return
+     */
+    public static float randomFloat(float min, float max) {
+        return (float) (random.nextDouble() * ((max - min) + 1)) + min;
+    }
+
+    /**
+     * Returns a random double between 0 (inclusive) and 1 (exclusive).
+     *
+     * @return
+     */
+    public static double randomDouble() {
+        return random.nextDouble();
+    }
+
+    /**
+     * Returns a random double within the specified range.
+     *
+     * @param min The minimum value (inclusive)
+     * @param max The maximum value (exclusive)
+     * @return
+     */
+    public static double randomDouble(double min, double max) {
+        return (random.nextDouble() * ((max - min) + 1)) + min;
+    }
+
+    /**
+     * Returns random bytes.
+     *
+     * @param length The number of bytes to return
+     * @return
+     */
+    public static byte[] randomBytes(int length) {
+        byte[] bytes = new byte[length];
+        random.nextBytes(bytes);
+        return bytes;
+    }
 }
